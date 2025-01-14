7514491ec90c51924d5ad73bd2dbc4587684eb7e
evertonprdo <ecpj.prado@gmail.com>
2025-01-14 12:15:52 -0300

    ObjectURL: save as file

--- vanilla/src/server.ts
+++ vanilla/src/server.ts
@@ -7,5 +7,34 @@ const filePath = path.join(dirPath, 'sample.png')
 const buffer = await fsPromises.readFile(filePath)
 const base64url = buffer.toString('base64url')
 
-const newFilePath = path.join(dirPath, 'test.png')
-await fsPromises.writeFile(newFilePath, base64url, { encoding: 'base64url' })
+const newFileTextPath = path.join(dirPath, 'sample.txt')
+const blobData = `data:image/png;base64,${base64url}`
+
+await fsPromises.writeFile(newFileTextPath, blobData)
+
+const blobReaderBuffer = await fsPromises.readFile(newFileTextPath, {
+   encoding: 'utf-8',
+})
+
+async function saveFileFromObjectURL(data: string) {
+   const regExp = /^data:([^;]+);base64,/
+   const match = data.match(regExp)
+
+   if (!match) {
+      throw new Error()
+   }
+
+   const mimeType = match[1]
+   const base64Data = data.split(',')[1]
+
+   const [type, ext] = mimeType.split('/')
+
+   if (type !== 'image') {
+      throw new Error()
+   }
+
+   const newFile = path.join(dirPath, `${new Date().getTime()}.${ext}`)
+   await fsPromises.writeFile(newFile, base64Data, { encoding: 'base64' })
+}
+
+await saveFileFromObjectURL(blobReaderBuffer)
