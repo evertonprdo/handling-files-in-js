6baae4a60af75b4a2e0c7e9b8cea7cd9134ba206
evertonprdo <ecpj.prado@gmail.com>
2025-01-14 11:59:51 -0300

    Enconding & Decoding: Base64

--- vanilla/src/server.ts
+++ vanilla/src/server.ts
@@ -2,18 +2,10 @@ import fsPromises from 'node:fs/promises'
 import path from 'node:path'
 
 const dirPath = path.join(import.meta.dirname, 'tmp')
-await fsPromises.mkdir(dirPath, { recursive: true })
+const filePath = path.join(dirPath, 'sample.png')
 
-for (let i = 0; i < 5; i++) {
-   const filePath = path.join(dirPath, `sample-${i}.txt`)
-   await fsPromises.writeFile(filePath, 'Hello', {
-      encoding: 'utf-8',
-   })
-}
+const buffer = await fsPromises.readFile(filePath)
+const base64url = buffer.toString('base64url')
 
-const filesInDir = await fsPromises.readdir(dirPath)
-
-for (const fileName of filesInDir) {
-   const filePath = path.join(dirPath, fileName)
-   await fsPromises.appendFile(filePath, ' World!', { encoding: 'utf-8' })
-}
+const newFilePath = path.join(dirPath, 'test.png')
+await fsPromises.writeFile(newFilePath, base64url, { encoding: 'base64url' })
