5a9999548aa1dd4f1e6933577110f41f589db2b8
evertonprdo <ecpj.prado@gmail.com>
2025-01-14 17:57:46 -0300

    POST img binary & base64

--- vanilla/src/server.ts
+++ vanilla/src/server.ts
@@ -2,6 +2,10 @@ import http from 'node:http'
 import fs from 'node:fs/promises'
 import path from 'node:path'
 
+import { getImgBase64, getImgBuffer } from './utils/get-image.ts'
+import { handleBase64Img, handleBinaryImg } from './utils/post-image.ts'
+import { handleFormData } from './utils/handle-form-data.ts'
+
 async function index(req: http.IncomingMessage, res: http.ServerResponse) {
    try {
       const htmlPath = path.join(import.meta.dirname, 'index.html')
@@ -28,40 +32,7 @@ async function favicon(req: http.IncomingMessage, res: http.ServerResponse) {
    }
 }
 
-async function getImgBuffer(
-   req: http.IncomingMessage,
-   res: http.ServerResponse,
-) {
-   try {
-      const faviconPath = path.join(import.meta.dirname, 'tmp', 'sample.png')
-      const buffer = await fs.readFile(faviconPath)
-
-      res.writeHead(200, { 'content-type': 'image/png' })
-      res.end(buffer)
-   } catch (error) {
-      res.writeHead(500, { 'content-type': 'text/plain' })
-      res.end()
-   }
-}
-
-async function getImgBase64(
-   req: http.IncomingMessage,
-   res: http.ServerResponse,
-) {
-   try {
-      const faviconPath = path.join(import.meta.dirname, 'tmp', 'sample.png')
-      const base64 = await fs.readFile(faviconPath, { encoding: 'base64' })
-
-      res.writeHead(200, { 'content-type': 'application/json' })
-      res.end(JSON.stringify({ image: base64, type: 'image/png' }))
-   } catch (error) {
-      console.log(error)
-      res.writeHead(500, { 'content-type': 'text/plain' })
-      res.end()
-   }
-}
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
    if (req.url === '/' && req.method === 'GET') {
       return index(req, res)
    }
@@ -76,6 +47,18 @@ const server = http.createServer((req, res) => {
 
    if (req.url === '/image-base64' && req.method === 'GET') {
       return getImgBase64(req, res)
+   }
+
+   if (req.url === '/image-base64' && req.method === 'POST') {
+      return handleBase64Img(req, res)
+   }
+
+   if (req.url === '/image-form-data' && req.method === 'POST') {
+      return handleFormData(req, res)
+   }
+
+   if (req.url === '/image-binary' && req.method === 'POST') {
+      return handleBinaryImg(req, res)
    }
 
    res.writeHead(404)
