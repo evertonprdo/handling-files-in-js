3901f6dc9421e2878ae5af4e03ed5bd1794dee14
evertonprdo <ecpj.prado@gmail.com>
2025-01-15 10:25:34 -0300

    Readable Stream: on 'data', pause(), resume()

--- vanilla/src/server.ts
+++ vanilla/src/server.ts
@@ -1,68 +1,17 @@
-import http from 'node:http'
-import fs from 'node:fs/promises'
+import fs from 'node:fs'
 import path from 'node:path'
 
-import { getImgBase64, getImgBuffer } from './utils/get-image.ts'
-import { handleBase64Img, handleBinaryImg } from './utils/post-image.ts'
-import { handleFormData } from './utils/handle-form-data.ts'
+const readableStream = fs.createReadStream(
+   path.join(import.meta.dirname, 'sample.txt'),
+   { highWaterMark: 15 },
+)
 
-async function index(req: http.IncomingMessage, res: http.ServerResponse) {
-   try {
-      const htmlPath = path.join(import.meta.dirname, 'index.html')
-      const html = await fs.readFile(htmlPath, { encoding: 'utf-8' })
-
-      res.writeHead(200, { 'content-type': 'text/html' })
-      res.end(html)
-   } catch (error) {
-      res.writeHead(500, { 'content-type': 'text/plain' })
-      res.end()
-   }
-}
-
-async function favicon(req: http.IncomingMessage, res: http.ServerResponse) {
-   try {
-      const faviconPath = path.join(process.cwd(), 'public', 'favicon.ico')
-      const buffer = await fs.readFile(faviconPath)
-
-      res.writeHead(200, { 'content-type': 'image/x-icon' })
-      res.end(buffer)
-   } catch (error) {
-      res.writeHead(500, { 'content-type': 'text/plain' })
-      res.end()
-   }
-}
-
-const server = http.createServer(async (req, res) => {
-   if (req.url === '/' && req.method === 'GET') {
-      return index(req, res)
-   }
-
-   if (req.url === '/favicon.ico' && req.method === 'GET') {
-      return favicon(req, res)
-   }
-
-   if (req.url === '/image-buffer' && req.method === 'GET') {
-      return getImgBuffer(req, res)
-   }
-
-   if (req.url === '/image-base64' && req.method === 'GET') {
-      return getImgBase64(req, res)
-   }
-
-   if (req.url === '/image-base64' && req.method === 'POST') {
-      return handleBase64Img(req, res)
-   }
-
-   if (req.url === '/image-form-data' && req.method === 'POST') {
-      return handleFormData(req, res)
-   }
-
-   if (req.url === '/image-binary' && req.method === 'POST') {
-      return handleBinaryImg(req, res)
-   }
-
-   res.writeHead(404)
-   res.end()
+readableStream.on('data', (chunk) => {
+   console.log(`Received: ${chunk.toString().split('\n')[0]}`)
+   readableStream.pause()
+   console.log('There will be no additional data for 1 second.\n')
+   setTimeout(() => {
+      console.log('Now data will start flowing again.\n')
+      readableStream.resume()
+   }, 1000)
 })
-
-server.listen(3333, () => console.log('Server running!'))
